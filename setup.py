4975912dd3fb8cf69304e48bf59b1684d199c968
Jason Ish <ish@unx.ca>
2017-10-26 12:08:55 -0600

    Rename idstools -> suricata-updater.
    Delete idstools scripts.
    Rename idstools-rulecat to suricata-updater.

--- setup.py
+++ setup.py
@@ -1,33 +1,27 @@
 from setuptools import setup
 
-import idstools
+import suricata
 
 setup(
-    name="idstools",
-    version=idstools.version,
-    description="IDS Utility Library",
+    name="suricata-updater",
+    version=suricata.version,
+    description="Suricata Update Tool",
     author="Jason Ish",
     author_email="ish@unx.ca",
     packages=[
-        "idstools",
-        "idstools.rulecat",
-        "idstools.rulecat.configs",
-        "idstools.scripts",
-        "idstools.compat",
-        "idstools.compat.argparse",
+        "suricata",
+        "suricata.rulecat",
+        "suricata.rulecat.configs",
+        "suricata.scripts",
+        "suricata.compat",
+        "suricata.compat.argparse",
     ],
-    url="https://github.com/jasonish/py-idstools",
+    url="https://github.com/jasonish/suricata-updater",
     license="BSD",
     classifiers=[
         'License :: OSI Approved :: BSD License',
     ],
     scripts = [
-        "bin/idstools-gensidmsgmap",
-        "bin/idstools-u2fast",
-        "bin/idstools-u2json",
-        "bin/idstools-rulecat",
-        "bin/idstools-u2eve",
-        "bin/idstools-eve2pcap",
-        "bin/idstools-rulemod",
+        "bin/suricata-update",
     ],
 )
